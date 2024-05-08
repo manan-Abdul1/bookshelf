a2da0d66e3bcb4fbfed3a73402e51f355a7407a4
Abdul <manansheikh117@gmail.com>
2024-05-09 02:44:46 +0500

    Add a reading, completed and plan to read categorizaion

--- components/BooksList/BookList.js
+++ components/BooksList/BookList.js
@@ -1,77 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BookCard from "../BookCard/BookCard";
 import tw from 'tailwind-styled-components';
-
-const mockBooks = [
-    {
-        title: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
-        publicationHouse: "Scribner",
-        publicationDate: "April 10, 1925",
-        genre: "Fiction",
-        publicationYear: 1925,
-        image:
-            "https://c4.wallpaperflare.com/wallpaper/852/219/709/the-great-gatsby-gold-minimalism-digital-art-wallpaper-preview.jpg",
-    },
-    {
-        title: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        publicationHouse: "J. B. Lippincott & Co.",
-        publicationDate: "July 11, 1960",
-        genre: "Novel",
-        publicationYear: 1960,
-        image:
-            "https://c4.wallpaperflare.com/wallpaper/511/751/733/movie-to-kill-a-mockingbird-wallpaper-preview.jpg",
-    },
-    {
-        title: "1984",
-        author: "George Orwell",
-        publicationHouse: "Secker & Warburg",
-        publicationDate: "June 8, 1949",
-        genre: "Dystopian",
-        publicationYear: 1949,
-        image: "https://wallpaperaccess.com/full/204497.jpg",
-    },
-    {
-        title: "The Catcher in the Rye",
-        author: "J.D. Salinger",
-        publicationHouse: "Little, Brown and Company",
-        publicationDate: "July 16, 1951",
-        genre: "Novel",
-        publicationYear: 1951,
-        image:
-            "https://c1.wallpaperflare.com/preview/538/645/254/book-focus-jerome-david-salinger-literature.jpg",
-    },
-    {
-        title: "The Hobbit",
-        author: "J.R.R. Tolkien",
-        publicationHouse: "George Allen & Unwin",
-        publicationDate: "September 21, 1937",
-        genre: "Fantasy",
-        publicationYear: 1937,
-        image:
-            "https://c4.wallpaperflare.com/wallpaper/368/146/434/the-lord-of-the-rings-rings-map-artwork-wallpaper-preview.jpg",
-    },
-    {
-        title: "Harry Potter and the Philosopher's Stone",
-        author: "J.K. Rowling",
-        publicationHouse: "Bloomsbury",
-        publicationDate: "June 26, 1997",
-        genre: "Fantasy",
-        publicationYear: 1997,
-        image: "https://wallpaper.dog/large/978497.jpg",
-    },
-    {
-        title: "The Lord of the Rings",
-        author: "J.R.R. Tolkien",
-        publicationHouse: "George Allen & Unwin",
-        publicationDate: "July 29, 1954",
-        genre: "Fantasy",
-        publicationYear: 1954,
-        image:
-            "https://c4.wallpaperflare.com/wallpaper/215/336/873/the-lord-of-the-rings-movies-wallpaper-preview.jpg",
-    },
-];
+import { mockBooks } from "@/utils/mockData";
 
 const Container = tw.div`
   flex
@@ -79,11 +9,27 @@ const Container = tw.div`
   justify-center
   max-w-[80%]
   mx-auto
+  mb-40
+`;
+
+const CategorySection = tw.div`
+  w-full
+  mt-5
+  mb-4
+`;
+
+const CategoryTitle = tw.h2`
+  mb-2
+`;
+
+const CategoryBooks = tw.div`
+  flex
+  flex-wrap
+  justify-center
 `;
 
 const SortSection = tw.div`
   w-full
-  mt-5
   mb-4
 `;
 
@@ -102,22 +48,52 @@ const SortSelect = tw.select`
 
 const BookList = ({ searchText }) => {
   const [sortCriteria, setSortCriteria] = useState('title');
+  const [readingBooks, setReadingBooks] = useState([]);
+  const [completedBooks, setCompletedBooks] = useState([]);
+  const [planToReadBooks, setPlanToReadBooks] = useState([]);
 
-  const sortedBooks = mockBooks.sort((a, b) => {
-    if (a[sortCriteria] < b[sortCriteria]) return -1;
-    if (a[sortCriteria] > b[sortCriteria]) return 1;
-    return 0;
-  });
+  useEffect(() => {
+    // Categorize books based on their status
+    const categorizedBooks = mockBooks.reduce((acc, book) => {
+      switch (book.status) {
+        case "Reading":
+          return { ...acc, reading: [...acc.reading, book] };
+        case "Completed":
+          return { ...acc, completed: [...acc.completed, book] };
+        case "Plan to Read":
+          return { ...acc, planToRead: [...acc.planToRead, book] };
+        default:
+          return acc;
+      }
+    }, { reading: [], completed: [], planToRead: [] });
 
-  const filteredBooks = sortedBooks.filter((book) => {
-    const searchFields = ['title', 'author', 'publicationHouse', 'genre'];
-    return searchFields.some(field =>
-      book[field].toLowerCase().includes(searchText.toLowerCase())
-    );
-  });
+    // Sort categorized books based on selected criteria
+    const sortCategorizedBooks = (books) => {
+      return books.sort((a, b) => {
+        if (a[sortCriteria] < b[sortCriteria]) return -1;
+        if (a[sortCriteria] > b[sortCriteria]) return 1;
+        return 0;
+      });
+    };
+
+    // Update state with sorted categorized books
+    setReadingBooks(sortCategorizedBooks(categorizedBooks.reading));
+    setCompletedBooks(sortCategorizedBooks(categorizedBooks.completed));
+    setPlanToReadBooks(sortCategorizedBooks(categorizedBooks.planToRead));
+  }, [sortCriteria]);
+
+  const filterBooks = (books) => {
+    return books.filter((book) => {
+      const searchFields = ['title', 'author', 'publicationHouse', 'genre'];
+      return searchFields.some(field =>
+        book[field].toLowerCase().includes(searchText.toLowerCase())
+      );
+    });
+  };
 
   return (
     <Container>
+      {/* Sort Dropdown */}
       <SortSection>
         <SortLabel htmlFor="sortCriteria">Sort by:</SortLabel>
         <SortSelect
@@ -132,18 +108,35 @@ const BookList = ({ searchText }) => {
         </SortSelect>
       </SortSection>
 
-      {filteredBooks.map((mockBook, index) =>
-        <BookCard
-          key={index}
-          title={mockBook.title}
-          author={mockBook.author}
-          publicationHouse={mockBook.publicationHouse}
-          publicationDate={mockBook.publicationDate}
-          genre={mockBook.genre}
-          publicationYear={mockBook.publicationYear}
-          image={mockBook.image}
-        />
-      )}
+      {/* Reading Books */}
+      <CategorySection>
+        <CategoryTitle>Reading</CategoryTitle>
+        <CategoryBooks>
+          {filterBooks(readingBooks).map((book, index) => (
+            <BookCard key={index} {...book} />
+          ))}
+        </CategoryBooks>
+      </CategorySection>
+
+      {/* Completed Books */}
+      <CategorySection>
+        <CategoryTitle>Completed</CategoryTitle>
+        <CategoryBooks>
+          {filterBooks(completedBooks).map((book, index) => (
+            <BookCard key={index} {...book} />
+          ))}
+        </CategoryBooks>
+      </CategorySection>
+
+      {/* Plan to Read Books */}
+      <CategorySection>
+        <CategoryTitle>Plan to Read</CategoryTitle>
+        <CategoryBooks>
+          {filterBooks(planToReadBooks).map((book, index) => (
+            <BookCard key={index} {...book} />
+          ))}
+        </CategoryBooks>
+      </CategorySection>
     </Container>
   );
 };
