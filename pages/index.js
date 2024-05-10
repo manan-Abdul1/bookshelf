3a8798800eb30c46ba2eff58d59924febb908d67
Abdul <manansheikh117@gmail.com>
2024-05-11 04:12:38 +0500

    Now get All UserBooks

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Toaster } from "react-hot-toast";
 import Dashboard from "@/components/Dashboard/Dashboard";
 import { useRouter } from "next/router";
 import { useAuth } from "@/context/AuthContext";
+import { GET_ALL_BOOKS_BY_USER_ID } from "@/utils/serverUrl";
+import axios from "axios";
 
 const Home = () => {
   const router = useRouter();
-  const {userData } = useAuth();
+  const { userData } = useAuth();
+  const [userBooks, setUserBooks] = useState([]);
   useEffect(() => {
     const accessToken = localStorage.getItem("accessToken");
 
@@ -14,6 +17,19 @@ const Home = () => {
       router.push("/login");
     }
   }, []);
+  const getAllBooksByUserId = async (userId) => {
+    try {
+      const response = await axios.get(`${GET_ALL_BOOKS_BY_USER_ID}/${userId}`);
+      setUserBooks(response.data);
+    } catch (error) {
+      console.error("Error fetching user Books:", error);
+    }
+  };
+  useEffect(() => {
+    if (userData.id) {
+      getAllBooksByUserId(userData.id);
+    }
+  }, [userData]);
 
   return (
     <>
--- utils/serverUrl.js
+++ utils/serverUrl.js
@@ -1,6 +1,12 @@
 
 const API_SERVER_BASE_URL = process.env.NEXT_PUBLIC_API_SERVER_BASE_URL;
 
+// BOOK ROUTES
+
 export const CREATE_NEW_BOOK = `${API_SERVER_BASE_URL}/api/book`;
 
+// USER BOOK ROUTES
+export const GET_ALL_BOOKS_BY_USER_ID = `${API_SERVER_BASE_URL}/api/userBook/getAllBooks`;
+
+// CLOUDINARY IMAGE ROUTE
 export const CLOUDNIARY_IMG_URL = "https://api.cloudinary.com/v1_1/dipdjdhic/image/upload";
