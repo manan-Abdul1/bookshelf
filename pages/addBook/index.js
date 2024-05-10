3a0ed887c4ca506dd3d18b4bed96ff3333d701c3
Abdul <manansheikh117@gmail.com>
2024-05-11 01:40:10 +0500

    Create add A book route

---
+++ pages/addBook/index.js
@@ -0,0 +1,12 @@
+import BookForm from '@/components/BookForm/BookForm'
+import React from 'react'
+
+const AddBook = () => {
+  return (
+    <>
+        <BookForm/>
+    </>
+  )
+}
+
+export default AddBook
